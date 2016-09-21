8cd754dc820138de6e74a6dbc25b6d88cbf239a6
denzalereese <dlr249@cornell.edu>
2016-09-21 15:49:13 -0700

    adds ui router and home template

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,21 @@
 angular
-  .module('movieSurfer', ['ngResource']);
+  .module('movieSurfer', ['ngResource', 'ui.router'])
+  .config(config);
+
+config.$inject = ['$stateProvider', '$locationProvider'];
+
+
+function config($stateProvider, $locationProvider) {
+  $stateProvider
+    .state('home', {
+      url:'/',
+      templateUrl: 'templates/movies.html',
+      controllerAs: 'moviesCtrl',
+      controller: 'moviesController'
+    });
+
+  $locationProvider.html5Mode({
+   enabled: true,
+   requireBase: false
+ });
+}
